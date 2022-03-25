9dc0994dc72d629d46ca2eec865a74bf9cc718db
Ashish <ashishgupt25@gmail.com>
2022-03-25 21:54:02 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+
+const App = () => {
+  return <Product/>;
+};
+
+export default App;
